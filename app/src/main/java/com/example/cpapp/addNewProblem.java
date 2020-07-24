a1cae29475b2deea6a50fd4d536a51d316ce2677
atharva-sarage <atharva.sarage@gmail.com>
2020-07-24 23:29:02 +0530

    autofocus removed on edit text

--- app/src/main/java/com/example/cpapp/addNewProblem.java
+++ app/src/main/java/com/example/cpapp/addNewProblem.java
@@ -18,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
